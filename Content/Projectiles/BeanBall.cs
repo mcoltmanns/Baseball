d411f8d820cc25dbf6f825bbe08d7f9772648777
Miles G <mgee2@caltech.edu>
2023-10-27 14:44:07 -0700

    unfinished beanball

--- Content/Projectiles/BeanBall.cs
+++ Content/Projectiles/BeanBall.cs
@@ -30,38 +30,42 @@ namespace Baseball.Content.Projectiles
 			Projectile.ai[0] += 1f; // Use a timer to wait 15 ticks before applying gravity.
 			if (Projectile.ai[0] < 60f)
 			{
+				Projectile.ai[2] = 0;
 				Projectile.ai[0] = 60f;
 				Projectile.velocity.Y = Projectile.velocity.Y + 0.1f;
 			}
 			else {
-				for(int i = 0; i < 200; i++)
-    			{
-       				NPC target = Main.npc[i];
-       				//If the npc is hostile
-				
-       				if(!target.friendly)
-       				{
-           				//Get the shoot trajectory from the projectile and target
-           				float shootToX = target.position.X + (float)target.width * 0.5f - Projectile.Center.X;
-           				float shootToY = target.position.Y - Projectile.Center.Y;
-           				float distance = (float)System.Math.Sqrt((double)(shootToX * shootToX + shootToY * shootToY));
+				if (Projectile.ai[2] == 0) {
+					for(int i = 0; i < 200; i++) {
+						NPC target = Main.npc[i];
+						if(!target.friendly && target.active) {
+							float shootToX = target.position.X + (float)target.width * 0.5f - Projectile.Center.X;
+           					float shootToY = target.position.Y - Projectile.Center.Y;
+           					float distance = (float)System.Math.Sqrt((double)(shootToX * shootToX + shootToY * shootToY));
+							if (distance < 480f) {
+								Projectile.ai[2] = 1;
+								Projectile.ai[1] = i;
+							}
+							
 
-           				//If the distance between the live targeted npc and the projectile is less than 480 pixels
-           				if(distance < 480f && !target.friendly && target.active)
-           				{
-               			//Divide the factor, 3f, which is the desired velocity
-               				distance = 3f / distance;
+						}
+					}
+				}
+				else {
+					NPC target = Main.npc[(int) Projectile.ai[1]];
+					float shootToX = target.position.X + (float)target.width * 0.5f - Projectile.Center.X;
+           			float shootToY = target.position.Y - Projectile.Center.Y;
+           			float distance = (float)System.Math.Sqrt((double)(shootToX * shootToX + shootToY * shootToY));
+					distance = 3f / distance;
    
-               				//Multiply the distance by a multiplier if you wish the projectile to have go faster
-               				shootToX *= distance * 5;
-               				shootToY *= distance * 5;
+               		//Multiply the distance by a multiplier if you wish the projectile to have go faster
+               		shootToX *= distance * 5;
+               		shootToY *= distance * 5;
 
-               				//Set the velocities to the shoot values
-               				Projectile.velocity.X = shootToX;
-               				Projectile.velocity.Y = shootToY;
-           				}
-       				}
-    			}
+               		//Set the velocities to the shoot values
+               		Projectile.velocity.X = shootToX;
+               		Projectile.velocity.Y = shootToY;
+				}
 			}
 		}
 	}
